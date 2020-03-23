f79ff16760dbb6739e272e87df8531f62e1e19b3
JessZHENG <43813897+JessZHENG@users.noreply.github.com>
2020-03-23 17:58:04 +0800

    数据库加了一些例脚本

--- src/main/java/com/example/coursecommunity/controller/MainController.java
+++ src/main/java/com/example/coursecommunity/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.example.coursecommunity.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
+
 
     /**
      * 重定向到LOGIN
@@ -20,6 +22,21 @@ public class MainController {
     @GetMapping("/login")//登录界面
     public ModelAndView testlogin(Model model){
         model.addAttribute("hello","HelloWorld!");
+        model.addAttribute("hello2","123");
         return new ModelAndView("login","Model",model);
     }
+
+    @GetMapping("/loginCheck")
+    public String checkLogin(String username,String password){
+        if(username.equals("张三") && password.equals("123")){
+            return "redirect:/index";
+        }else
+        return "redirect:/login";
+    }
+
+    @GetMapping("/index")//登录界面
+    public ModelAndView testlogin(){
+        return new ModelAndView("index");
+    }
+
 }
---
+++ src/main/java/com/example/coursecommunity/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.coursecommunity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/index")//登录界面
+    public ModelAndView testlogin(){
+        return new ModelAndView("index");
+    }
+}
