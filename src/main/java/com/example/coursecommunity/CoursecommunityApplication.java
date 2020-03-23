23de98f122cb10f77b4d38b827fa77170cd98da4
JessZHENG <43813897+JessZHENG@users.noreply.github.com>
2020-03-23 20:32:00 +0800

    调好了前端文件夹的配置

--- src/main/java/com/example/coursecommunity/CoursecommunityApplication.java
+++ src/main/java/com/example/coursecommunity/CoursecommunityApplication.java
@@ -2,12 +2,22 @@ package com.example.coursecommunity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class CoursecommunityApplication {
+public class CoursecommunityApplication extends WebMvcConfigurationSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(CoursecommunityApplication.class, args);
+    }
+
+    //这里配置静态资源文件的路径导包都是默认的直接导入就可以
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+        super.addResourceHandlers(registry);
     }
 
 }
