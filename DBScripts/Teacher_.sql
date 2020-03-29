108807f3f88ebccae519be8045330ffdeef61f78
JessZHENG <43813897+JessZHENG@users.noreply.github.com>
2020-03-29 17:02:08 +0800

    SpringSecurity配置完毕！

---
+++ DBScripts/Teacher_.sql
@@ -0,0 +1,2 @@
+INSERT INTO coursecommunity.teacher (id, name, orgId, tCode, title) VALUES (1, '肖明忠', 2, '07041', '副教授');
+INSERT INTO coursecommunity.teacher (id, name, orgId, tCode, title) VALUES (2, '卫志民', 1, '04105', '教授');
